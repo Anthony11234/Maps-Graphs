901b1267fffae6f690727d1e65a99655104a7bd9
Adam Gonzalez <adamgonzalez005@gmail.com>
2017-12-17 20:37:34 -0800

    Main file using the graph functions

--- Code/main.cpp
+++ Code/main.cpp
@@ -18,22 +18,22 @@ int main(int argc, const char * argv[]) {
     int k = 0;
 
     //Graph Example #1
-    myGraph->addVertex(1);
-    myGraph->addVertex(2);
-    myGraph->addVertex(3);
-    myGraph->addVertex(4);
-    myGraph->addVertex(5);
-    myGraph->addVertex(6);
-    myGraph->addVertex(7);
-    myGraph->addVertex(8);
+    // myGraph->addVertex(1);
+    // myGraph->addVertex(2);
+    // myGraph->addVertex(3);
+    // myGraph->addVertex(4);
+    // myGraph->addVertex(5);
+    // myGraph->addVertex(6);
+    // myGraph->addVertex(7);
+    // myGraph->addVertex(8);
 
-    myGraph->addEdgeUnDir(1, 3);
-    myGraph->addEdgeUnDir(1, 2);
-    myGraph->addEdgeUnDir(2, 4);
-    myGraph->addEdgeUnDir(2, 5);
-    myGraph->addEdgeUnDir(2, 6);
-    myGraph->addEdgeUnDir(3, 7);
-    myGraph->addEdgeUnDir(3, 8);
+    // myGraph->addEdgeUnDir(1, 3);
+    // myGraph->addEdgeUnDir(1, 2);
+    // myGraph->addEdgeUnDir(2, 4);
+    // myGraph->addEdgeUnDir(2, 5);
+    // myGraph->addEdgeUnDir(2, 6);
+    // myGraph->addEdgeUnDir(3, 7);
+    // myGraph->addEdgeUnDir(3, 8);
     //End Graph Example #1
 
     //Graph Example #2
@@ -80,6 +80,18 @@ int main(int argc, const char * argv[]) {
 //    myGraph->addEdgeUnDir(5, 6);
 //    myGraph->addEdgeUnDir(6, 7);
     //End Graph Example #3
+
+    myGraph -> addVertex(0);
+    myGraph -> addVertex(1);
+    myGraph -> addVertex(2);
+    myGraph -> addVertex(3);
+    myGraph -> addVertex(4);
+
+    myGraph -> addEdgeUnDir(0,1);
+    myGraph -> addEdgeUnDir(0,2);
+    myGraph -> addEdgeUnDir(1,3);
+    myGraph -> addEdgeUnDir(2,3);
+    myGraph -> addEdgeUnDir(3,4);
 
     printGraph(myGraph);
 
